9100ba86de8b707b0e83895ffb6606cb48ffb253
seobity <codemin1010@gmail.com>
2025-10-12 13:54:29 +0900

    feat: 댓글 기능과 DB 연결

--- PWP_tutoring/sns/comments/views.py
+++ PWP_tutoring/sns/comments/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Comment
 
 def comment_view(request):
-    comment = None  
+    if request.method == 'POST': 
+        content = request.POST.get('content')
+        Comment.objects.create(content=content)
+        return redirect('comments:comment')
+
+    comments = Comment.objects.all().order_by('-created_at')
+    return render(request, 'comments/comment.html', {'comments': comments})
+
+
+
+
+# from django.shortcuts import render
+
+# def comment_view(request):
+#     comment = None  
     
-    if request.method == 'POST':
-        comment = request.POST.get('comment_text')
+#     if request.method == 'POST':
+#         comment = request.POST.get('comment_text')
         
         
-    return render(request, 'comments/comment.html', {'comment' : comment})
+#     return render(request, 'comments/comment.html', {'comment' : comment})
